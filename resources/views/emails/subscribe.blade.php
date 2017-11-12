bf098bd7b75bf7458f54cc2ec47d6f4042ee8bb7
It'$ MYTH <It'$ MYTH>
2017-11-12 08:39:40 +0530

    Email content changed

--- resources/views/emails/subscribe.blade.php
+++ resources/views/emails/subscribe.blade.php
@@ -5,51 +5,57 @@
   <table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color:#f3f2f0;" bgcolor="#f3f2f0;">
     <tr>
       <td width="100%"><div class="webkit" style="max-width:600px;Margin:0 auto;"> 
-          <table class="outer" align="center" cellpadding="0" cellspacing="0" border="0" style="border-spacing:0;Margin:0 auto;width:100%;max-width:600px;">
-                  <tr>
-                    <td height="30">&nbsp;</td>
-                  </tr>
-                  <tr>
-                    <td class="two-column" style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;text-align:center;font-size:0;">
-                     </td>
-                  </tr>
-                  <tr>
-                    <td height="30">&nbsp;</td>
-                  </tr>
-            <tr>
-
-              <td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;">
-                <table class="one-column" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-spacing:0; border-left:1px solid #e8e7e5; border-right:1px solid #e8e7e5; border-bottom:1px solid #e8e7e5; border-top:1px solid #e8e7e5" bgcolor="#FFFFFF">
-                  <tr>
-
-                    <td align="left" style="padding:50px 50px 50px 50px">
-                      <img src="{{ $message->embed(public_path() . '/images/logo.png') }}" style="width:100px" /><p style="color:#262626; font-size:24px; text-align:left; font-family: Verdana, Geneva, sans-serif">
-                       <strong>Dear {{ $full_name }}</strong>,</p>
-                      <p style="color:#000000; font-size:16px; text-align:left; font-family: Verdana, Geneva, sans-serif; line-height:22px ">Thank you for showing interest in India's very first bitcoin trading exchange platform . We will let you know the updates about the same. Wait for the most exciting automated trade and be a trader with Miner Junction<br />
-                      </p>
-                      <p style="color:#000000; font-size:16px; text-align:left; font-family: Verdana, Geneva, sans-serif; line-height:22px ">Your subscription code is <strong>{{ $code }}</strong>.</p>
-                      <p style="color:#000000; font-size:16px; text-align:left; font-family: Verdana, Geneva, sans-serif; line-height:22px ">Kindly use it at the time of registration to avail discount.</p>
-                      <p style="color:#000000; font-size:16px; text-align:left; font-family: Verdana, Geneva, sans-serif; line-height:22px ">
-                        <br />
-                        Best Regards, <br />
-                        Miner Junction & Team</p></td>
-                  </tr>
-                </table>
-                <table cellpadding="0" cellspacing="0" border="0" width="100%">
-                  <tr>
-                    <td height="30">&nbsp;</td>
-                  </tr>
-                  <tr>
-                    <td class="two-column" style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;text-align:center;font-size:0;">
-                     </td>
-                  </tr>
-                  <tr>
-                    <td height="30">&nbsp;</td>
-                  </tr>
-                </table>
-                </td>
-            </tr>
-        </div></td>
+        <table class="outer" align="center" cellpadding="0" cellspacing="0" border="0" style="border-spacing:0;Margin:0 auto;width:100%;max-width:600px;">
+          <tr>
+            <td height="30">&nbsp;</td>
+          </tr>
+          <tr>
+            <td class="two-column" style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;text-align:center;font-size:0;">
+              </td>
+          </tr>
+          <tr>
+            <td height="30">&nbsp;</td>
+          </tr>
+          <tr>
+            <td style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;">
+              <table class="one-column" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-spacing:0; border-left:1px solid #e8e7e5; border-right:1px solid #e8e7e5; border-bottom:1px solid #e8e7e5; border-top:1px solid #e8e7e5" bgcolor="#FFFFFF">
+                <tr>
+                  <td align="left" style="padding:50px 50px 50px 50px">
+                    <img src="{{ $message->embed(public_path() . '/images/logo.png') }}" style="width:100px" /><p style="color:#262626; font-size:24px; text-align:left; font-family: Verdana, Geneva, sans-serif">
+                      <strong>Dear {{ $full_name }}</strong>,</p>
+                    <p style="color:#00000000000; font-size:16px; text-align:left; font-family: Verdana, Geneva, sans-serif; line-height:22px ">
+                      Thank you for showing interest in India's very first bitcoin cloud mining platform. We will let you know the updates about the same. Wait for the most exciting automated mining with Miner Junction.<br />
+                    </p>
+                    <p style="color:#000000; font-size:16px; text-align:left; font-family: Verdana, Geneva, sans-serif; line-height:22px ">
+                      Your subscription code is <strong>{{ $code }}</strong>.
+                    </p>
+                    <p style="color:#000000; font-size:16px; text-align:left; font-family: Verdana, Geneva, sans-serif; line-height:22px ">
+                      Kindly use it at the time of registration to avail discount.
+                    </p>
+                    <p style="color:#000000; font-size:16px; text-align:left; font-family: Verdana, Geneva, sans-serif; line-height:22px ">
+                      <br />
+                      Best Regards, <br />
+                      Miner Junction & Team
+                    </p>
+                  </td>
+                </tr>
+              </table>
+              <table cellpadding="0" cellspacing="0" border="0" width="100%">
+                <tr>
+                  <td height="30">&nbsp;</td>
+                </tr>
+                <tr>
+                  <td class="two-column" style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;text-align:center;font-size:0;">
+                    </td>
+                </tr>
+                <tr>
+                  <td height="30">&nbsp;</td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </td>
     </tr>
   </table>
 </center>
